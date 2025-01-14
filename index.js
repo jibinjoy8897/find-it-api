c209e4687ad2058b4b8e989cad6b7b70f5748971
jibinjoy8897 <jibinjoy8897@gmail.com>
2025-01-13 21:50:53 -0800

    CORS fix

--- index.js
+++ index.js
@@ -5,29 +5,15 @@ const cors = require("cors");
 
 app.use(cors());
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	res.header('Access-Control-Allow-Credentials', 'true');
+	if (req.method === 'OPTIONS') {
+	  return res.sendStatus(200); // Handle preflight requests correctly
+	}
+	next();
 });
 
 const PORT = process.env.PORT || 5000;
