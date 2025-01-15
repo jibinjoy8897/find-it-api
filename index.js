90dc62f5127d27af936a0421de9b9c501f5823c0
jibinjoy8897 <jibinjoy8897@gmail.com>
2025-01-14 21:54:06 -0800

    optimize cors fix

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-console.log("ENV CLIENT",process.env.FRONTEND_URL);
-app.use(
-  cors({
-    origin: ["http://localhost:3001", process.env.FRONTEND_URL], // Set allowed origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // Allow cookies
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3001", process.env.FRONTEND_URL], // Set allowed origins
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true, // Allow cookies
+//   })
+// );
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
