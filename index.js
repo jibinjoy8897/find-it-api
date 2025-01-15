04d3e271bc9c47926febf7b61622f04d3cdee4d7
jibinjoy8897 <jibinjoy8897@gmail.com>
2025-01-14 21:48:30 -0800

    CORS fix 6th try

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ console.log("ENV CLIENT",process.env.FRONTEND_URL);
 app.use(
   cors({
     origin: ["http://localhost:3001", process.env.FRONTEND_URL], // Set allowed origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, // Allow cookies
   })
 );
@@ -16,5 +17,5 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello from our server!");
+  res.send("Hello from our server! Hi");
 });
