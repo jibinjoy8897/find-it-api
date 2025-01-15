adc3b8ff8204cee6851bd68f2fe9706d4b2c148a
jibinjoy8897 <jibinjoy8897@gmail.com>
2025-01-14 21:45:16 -0800

    ENV client url check

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// console.log("app>>", app);
 
-const options = [
+console.log("ENV CLIENT",process.env.FRONTEND_URL);
+app.use(
   cors({
-    origin: '*',
-    methods: '*',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+    origin: ["http://localhost:3001", process.env.FRONTEND_URL], // Set allowed origins
+    credentials: true, // Allow cookies
   })
-];
-
-app.use(options);
-
-// app.use(cors({
-// 	origin: ["http://localhost:3001", process.env.FRONTEND_URL], // Set allowed origins
-// 	credentials: true, // Allow cookies
-// }));
-
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-	res.header('Access-Control-Allow-Credentials', 'true');
-	if (req.method === 'OPTIONS') {
-	  return res.sendStatus(200); // Handle preflight requests correctly
-	}
-	next();
-});
-
-
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
